b07f6204b6c200b8beb47bbab0b9c8d076f3ffda
lipan <areslipan@163.com>
2014-07-13 16:00:53 +0800

    spring mvc + velocity 的helloworld
    Signed-off-by: lipan <areslipan@163.com>

---
+++ HelloWorld/src/main/java/com/lipan/spring/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.lipan.spring;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/helloworld")
+public class HelloWorld {
+
+    @RequestMapping(value="/index", method = {RequestMethod.GET})
+    public ModelAndView index(){
+        
+        ModelAndView modelAndView = new ModelAndView();  
+        modelAndView.addObject("message", "Hello World!");  
+        modelAndView.setViewName("index");  
+        return modelAndView;
+    }
+    
+}
